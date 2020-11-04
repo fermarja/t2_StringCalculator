ea4e46bb82531c573bd872475617dc33ab63f43e
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-04 09:56:16 +0100

    Refactor: separa la extracción de los numeros y la operacion de suma

--- src/main/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculator.java
+++ src/main/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculator.java
@@ -12,12 +12,26 @@ public class StringCalculator {
 		if (StringUtils.isBlank(numbers)) {
 			return 0;
 		}
-		String[] numeros = numbers.split(",");
+		int[] numeros = extraerNumeros(numbers);
+		return sumar(numeros);
+		
+	}
+
+	private int sumar(int[] numeros) {
 		int suma = 0;
-		for (String numero : numeros) {
-			suma += Integer.parseInt(numero);
+		for (int numero : numeros) {
+			suma += numero;
 		}
 		return suma;
+	}
+
+	private int[] extraerNumeros(String numbers) {
+		String[] numeros = numbers.split(",");
+		int[] resultado = new int[numeros.length];
+		for(int i = 0; i < numeros.length; i++) {
+			resultado[i] = Integer.parseInt(numeros[i]);
+		}
+		return resultado;
 	}
 
 }
