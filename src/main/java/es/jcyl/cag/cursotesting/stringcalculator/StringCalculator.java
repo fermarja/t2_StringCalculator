ae4eaae1144235a898c47959e4eb2a659e55b299
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-04 13:35:36 +0100

    Refactor: usa nuevo metodo para extraer separadores

--- src/main/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculator.java
+++ src/main/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculator.java
@@ -31,12 +31,22 @@ public class StringCalculator {
 	}
 
 	private int[] extraerNumeros(String numbers) {
-		String[] separadores = new String[]{",", "\n"};
 		String parteDeNumeros = numbers;
+		 if (numbers.startsWith("//")) {
+			 parteDeNumeros = numbers.substring(numbers.indexOf('\n')+1);
+		 }
+		String[] separadores = extraerSeparadores(numbers);
+		return extraerNumeros(parteDeNumeros, separadores);
+	}
+	
+	
+
+	private String[] extraerSeparadores(String numbers) {
+		String[] separadores = new String[]{",", "\n"};
+		
 		if (numbers.startsWith("//[")) {
 			String tmp = numbers.substring(3, numbers.indexOf('\n')- 1);
 			separadores = StringUtils.splitByWholeSeparator(tmp, "][");
-			parteDeNumeros = numbers.substring(numbers.indexOf('\n')+1);
 		}
 		else if (numbers.startsWith("//")) {
 			String separadoresTmp = numbers.substring(2, numbers.indexOf('\n'));
@@ -46,9 +56,8 @@ public class StringCalculator {
 			else {
 				separadores = new String[] {separadoresTmp};
 			}
-			parteDeNumeros = numbers.substring(numbers.indexOf('\n')+1);
 		}
-		return extraerNumeros(parteDeNumeros, separadores);
+		return separadores;
 	}
 
 	private int[] extraerNumeros(String parteDeNumeros, String[] separadores) {
