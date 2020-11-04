f440bac37baee2f58330298adfcc32757aef8017
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-04 09:44:38 +0100

    Test cadena vacia y null

--- src/test/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculatorTest.java
+++ src/test/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculatorTest.java
@@ -1,5 +1,30 @@
 package es.jcyl.cag.cursotesting.stringcalculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringCalculatorTest {
+	
+	public StringCalculatorTest () {
+		super();
+	}
+	
+	@Test
+	public void testNull() {
+		Assert.assertEquals(0, add(null));
+	}
+	@Test
+	public void testCadenaVacia() {
+		Assert.assertEquals(0, add(""));
+	}
+	@Test
+	public void testCadenaBlank() {
+		Assert.assertEquals(0, add("   "));
+	}
+	
+	
+	private int add(String numbers) {
+		return new StringCalculator().add(numbers);
+	}
 
 }
