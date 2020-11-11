103b1c4bd05154e73489c8c6e709a6b571b94686
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-11 18:22:41 +0100

    Eliminamos test iniciales.
    Los test iniciales probaban a la vez el parseo de cadenas con la
    funcionalidad propia de string calculator de sumar numeros.
    Ahora los test de cadenas están en DefaultExtraeNumerosTest y en esta
    clase quedan los test de la calcuadora con unos stubs
    
    Fixes #2

--- src/test/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculatorTest.java
+++ src/test/java/es/jcyl/cag/cursotesting/stringcalculator/StringCalculatorTest.java
@@ -12,18 +12,6 @@ public class StringCalculatorTest {
 		super();
 	}
 	
-	@Test
-	public void devolverCeroSiElParametroEsNull() {
-		Assert.assertEquals(0, add((String) null));
-	}
-	@Test
-	public void devolverCeroSiElParametroEsVacio() {
-		Assert.assertEquals(0, add(""));
-	}
-	@Test
-	public void devolverCeroSiElParametroEsBlancos() {
-		Assert.assertEquals(0, add("   "));
-	}
 	
 	@Test
 	public void sumaDeNingunValorEsCero() {
@@ -36,44 +24,15 @@ public class StringCalculatorTest {
 	}
 	
 	@Test
-	public void sumaDeUnUnicoNumeroEsElMismo() {
-		Assert.assertEquals(2, add("2"));
-	}
-	
-	@Test
 	public void sumaDeTresYDosEsCinco() {
 		Assert.assertEquals(5, add(3,2));
-	}
-	
-	@Test
-	public void sumaDosNumeros() {
-		Assert.assertEquals(5, add("3,2"));
 	}
 	
 	@Test
 	public void sumaDeMasDeTresNumeros() {
 		Assert.assertEquals(16, add(1,2,3,10));
 	}
-	
-	@Test
-	public void sumaMasDeDosNumeros() {
-		Assert.assertEquals(16, add("1,2,3,10"));
-	}
-	
-	@Test
-	public void delimitadorSaltoDeLinea() {
-		Assert.assertEquals(6,  add("1\n2,3"));
-	}
-	
-	@Test
-	public void delimitadorPersonalizado() {
-		Assert.assertEquals(3, add("//;\n1;2"));
-	}
-	
-	@Test
-	public void delimitadorPersonalizadoPeroVacio() {
-		Assert.assertEquals(3, add("//\n1;2"));
-	}
+
 	
 	@Test
 	public void noSePermitenNegativos() {
@@ -87,47 +46,13 @@ public class StringCalculatorTest {
 			Assert.assertTrue(msg.contains("-1, -5, -20"));
 		}
 	}
-	@Test
-	public void noSePermitenNumerosNegativos() {
-		try {
-			add("1,-1,-5,10,-20");
-			Assert.fail("Debe lanzarse la excepcion");
-		}
-		catch (IllegalArgumentException e) {
-			String msg = e.getMessage();
-			Assert.assertTrue(msg.contains("negativos no soportados"));
-			Assert.assertTrue(msg.contains("-1, -5, -20"));
-		}
-	}
+
 	
 	@Test
 	public void seIgnoranEnLaSumaNumerosMayoresDeMil() {
 		Assert.assertEquals(2, add(2, 1002));
 	}
-	
-	@Test
-	public void noSePermitenNumerosMayoresDeMil() {
-		Assert.assertEquals(2, add("2,1002"));
-	}
-	
-	@Test
-	public void separadorMultiplesCaracteres() {
-		Assert.assertEquals(6, add("//[***]\n1***2***3"));
-	}
-	
-	@Test
-	public void variosSeparadoresDeMultiplesCaracteresUnCaracterCadaUno() {
-		Assert.assertEquals(6, add("//[*][%]\n1*2%3"));
-	}
-	
-	@Test
-	public void variosSeparadoresDeMultiplesCaracteresDinstintaLongitud() {
-		Assert.assertEquals(6, add("//[*****][%%%]\n1*****2%%%3"));
-	}
-	
-	private int add(String numbers) {
-		return new StringCalculator().add(numbers);
-	}
+
 	
 	private int add(int... numeros) {
 		ExtraeNumeros extractor = mock(ExtraeNumeros.class);
